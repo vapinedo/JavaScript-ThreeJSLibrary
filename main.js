cf83449738ae5231d12a42404138da16de2fe99d
Victor Pinedo <victorpinedomartinez@gmail.com>
2023-09-03 15:31:09 -0500

    getting started

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const rendered = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 
-rendered.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(rendered.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
